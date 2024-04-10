be924c14aacc2fd5278562f3c1d50752b720219c
Vihaan Shah <65532341+Vihaanshah29@users.noreply.github.com>
2024-04-10 13:18:36 -0400

    first commit

---
+++ src/main/java/edu/iu/prebello/primeservice/controller/PrimesController.java
@@ -0,0 +1,31 @@
+package edu.iu.prebello.primeservice.controller;
+
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.oauth2.jwt.Jwt;
+import edu.iu.prebello.primeservice.rabbitmq.MQSender;
+import edu.iu.prebello.primeservice.service.IprimesService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
+public class PrimesController {
+
+    IprimesService iprimesService;
+    private final MQSender mqSender;
+
+    public PrimesController(IprimesService iprimesService, MQSender mqSender) {
+        this.iprimesService = iprimesService;
+        this.mqSender = mqSender;
+    }
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable int n) {
+        boolean result = iprimesService.isPrime(n);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqSender.sendMessage(username, n, result);
+        return result;
+    }
+}
